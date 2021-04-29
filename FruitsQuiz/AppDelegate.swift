52b1e868c76bddda1a5f6c692f67a835b209f355
yasinoki39 <yasinoki39@gmail.com>
2021-04-29 21:13:02 +0900

    other

--- FruitsQuiz/AppDelegate.swift
+++ FruitsQuiz/AppDelegate.swift
@@ -14,7 +14,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
       // Google Mobile Ads SDKの初期化
         GADMobileAds.sharedInstance().start(completionHandler: nil) // 追加
-      
       return true
     }
 
@@ -31,5 +30,4 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
         // If any sessions were discarded while the application was not running, this will be called shortly after application:didFinishLaunchingWithOptions.
         // Use this method to release any resources that were specific to the discarded scenes, as they will not return.
     }
-
 }
